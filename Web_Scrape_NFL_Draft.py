f4ddf24333da3f31e8eefa17df5c954bb49e7047
jpf5046 <jfriel1014@gmail.com>
2018-12-15 16:01:55 -0600

    Add files via upload

--- Web_Scrape_NFL_Draft.py
+++ Web_Scrape_NFL_Draft.py
@@ -2,17 +2,15 @@
 import pandas as pd
 
 url_template = "http://www.pro-football-reference.com/years/{year}/draft.htm"
-# for each year from 1967 to (and including) 2016
+# for each year from 1950 to 2017
 dataset = []
 for year in range(1950, 2017):
     df_list = pd.read_html(url_template.replace("{year}",str(year)), skiprows = 0)[0]
     dataset.append(df_list)
 dataset = pd.concat(dataset)
 
-
 # removing first row of multi-header
 dataset.columns = dataset.columns.droplevel(0)
-
 
 # removing all the multiple headers
 dataset = dataset[dataset.Player <> 'Player']
