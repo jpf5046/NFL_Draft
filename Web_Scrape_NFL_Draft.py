55f9c7ca4ceede89670a0f6647ad0a10b7380367
jpf5046 <jfriel1014@gmail.com>
2019-01-25 11:18:43 -0600

    Update Web_Scrape_NFL_Draft.py
    Added `header` option to `read_html` to pull in the correct header index. Added "Draft Year" to the final table.

--- Web_Scrape_NFL_Draft.py
+++ Web_Scrape_NFL_Draft.py
@@ -1,19 +1,16 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
 
 url_template = "http://www.pro-football-reference.com/years/{year}/draft.htm"
 # for each year from 1950 to 2017
 dataset = []
 for year in range(1950, 2017):
-    df_list = pd.read_html(url_template.replace("{year}",str(year)), skiprows = 0)[0]
+    df_list = pd.read_html(url_template.replace("{year}",str(year)), skiprows = 0, header=1,  index_col=1)[0]
+    df_list['Draft Year']= year
     dataset.append(df_list)
 dataset = pd.concat(dataset)
 
-# removing first row of multi-header
-dataset.columns = dataset.columns.droplevel(0)
-
 # removing all the multiple headers
-dataset = dataset[dataset.Player <> 'Player']
+dataset = dataset[dataset.Player != 'Player']
 
 # export to excel
 dataset.to_excel('NFL_Draft_1950_to_2017.xlsx')
